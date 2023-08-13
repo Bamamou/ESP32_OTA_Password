a0d9b3a37eb20e68f0bb657f0fb6fef3e5272733
bamamounicolas <bamamounicolas@gmail.com>
2023-08-13 10:37:31 +0800

    This is the first commit with the little info on the website and login info

---
+++ include/Global_Const.h
@@ -0,0 +1,10 @@
+#ifndef __GLOBAL_CONST_H__
+#define ___GLOBAL_CONST_H__
+
+#include<Arduino.h>
+
+const char* host = "evoke";
+const char* ssid = "FLYRouter-e920";
+const char* password = "b33cd675";
+
+#endif
---
+++ include/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include<Arduino.h>
+#include<Global_Const.h>
+#include<WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
+
+#endif
